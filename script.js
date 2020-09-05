e8b86a7cb7a916a2af7f9fb22dbb9220d73d2359
Christian Poon <christianxp27@yahoo.com>
2020-09-04 22:27:37 -0700

    update let variables to const and use arrow functions

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-(function() {
+(function () {
   // Select DOM elements
-  let option = document.querySelector('#metric-select');
-  let fact = document.querySelector('#fact');
-  let factText = document.querySelector('#factText');
+  const option = document.querySelector('#metric-select');
+  const fact = document.querySelector('#fact');
+  const factText = document.querySelector('#factText');
 
-  let numberInput = document.querySelector('#numberInput');
-  let dayInput = document.querySelector('#dayInput');
+  const numberInput = document.querySelector('#numberInput');
+  const dayInput = document.querySelector('#dayInput');
 
   // Initialize Numbers API URL and optionString
-  let url = 'http://numbersapi.com/'
+  const url = 'http://numbersapi.com/';
   let optionString = '';
 
   // 1. Hide display
   // 2. Change the placeholder for the first input if the option is "date"
   // 3. Reset all input values
-  let resetDisplay = function() {
+  const resetDisplay = () => {
     fact.style.display = 'none';
-    optionString === 'date' ? numberInput.setAttribute('placeholder', "Enter a month...") : numberInput.setAttribute('placeholder', 'Enter any number...');
+    optionString === 'date'
+      ? numberInput.setAttribute('placeholder', 'Enter a month...')
+      : numberInput.setAttribute('placeholder', 'Enter any number...');
     numberInput.value = '';
     dayInput.value = '';
-  }
+  };
 
   // 1. Set the optionString every time the user changes the option
   // 2. If the option is "date", then display the second text input
   // 3. Reset the display
-  let configureOption = function(event) {
-    let selectedOption = event.target.value;
+  const configureOption = event => {
+    const selectedOption = event.target.value;
     if (selectedOption === 'trivia') {
       optionString = '';
     } else {
@@ -37,11 +39,11 @@
       optionString = selectedOption;
     }
     resetDisplay();
-  }
+  };
 
   // Generate a url and a flag to determine if the url is valid
-  let generateValidUrl = function() {
-    let number = numberInput.value; // This number will be the month if the option is date
+  const generateValidUrl = () => {
+    const number = numberInput.value; // This number will be the month if the option is date
     let newUrl = url;
     let valid = true;
 
@@ -52,7 +54,7 @@
       } else {
         valid = false;
         factText.style.display = 'block';
-        factText.innerText = "Please input a value for both inputs";
+        factText.innerText = 'Please input a value for both inputs';
       }
     } else {
       if (number === '') {
@@ -67,39 +69,39 @@
     }
 
     return [newUrl, valid];
-  }
+  };
 
   // Option 1: use Ajax to asynchronously retrieve data from the url
-  let getFactAjax = function() {
-    let [newUrl, valid] = generateValidUrl();
-    
-    let xhr = new XMLHttpRequest();
+  const getFactAjax = () => {
+    const [newUrl, valid] = generateValidUrl();
+
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', newUrl);
 
-    xhr.onload = function() {
+    xhr.onload = () => {
       if (this.status === 200 && valid) {
         fact.style.display = 'block';
         factText.innerText = this.responseText;
       }
-    }
+    };
 
     xhr.send();
-  }
+  };
 
   // Option 2: use Fetch API to asynchronously retrieve data from the url
-  let getFactFetch = function() {
-    let [newUrl, valid] = generateValidUrl();
+  const getFactFetch = () => {
+    const [newUrl, valid] = generateValidUrl();
 
     if (valid) {
       fetch(newUrl)
-      .then(res => res.text())
-      .then(data => {
-        fact.style.display = 'block';
-        factText.innerText = data;
-      })
-      .catch(err => console.log(err));
+        .then(res => res.text())
+        .then(data => {
+          fact.style.display = 'block';
+          factText.innerText = data;
+        })
+        .catch(err => console.log(err));
     }
-  }
+  };
 
   option.addEventListener('input', configureOption);
 
